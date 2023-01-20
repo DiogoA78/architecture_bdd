0712c03b87958bada5a253ef130253c99748f35f
Diogo Almeida <diogo.m.almeida@outlook.fr>
2023-01-20 15:36:05 +0100

    ouiiii

--- dataset/EL/el.py
+++ dataset/EL/el.py
@@ -10,4 +10,5 @@ new_colonnes.to_csv(r"dataset\EL\el_stats_clubs_attack.csv", index=False)
 df2 = pd.read_csv(r"dataset\EL\el_stats_players_attack.csv")
 new_colonnes2 = df2["team"].str.split(" ", expand=True)
 new_colonnes2.columns = ["Rank", "Nom", "Equipe", "Tiret", "Poste", "Assist", "Corner_taken", "Offside", "Match_played"]
+new_colonnes2.drop("Tiret", axis=1, inplace=True)
 new_colonnes2.to_csv(r"dataset\EL\el_stats_players_attack.csv", index=False)
--- dataset/LDC/ucl.py
+++ dataset/LDC/ucl.py
@@ -3,11 +3,12 @@ import pandas as pd
 df = pd.read_csv(r"dataset\LDC\ucl_clubs_attack.csv")
 new_colonnes = df["team"].str.split(" ", expand=True)
 new_colonnes.columns = ["Rank", "Equipe", "Country", "Attack", "Assist", "Corner_taken", "Offside", "Dribble", "Match_played"]
-df.drop("team", axis=1, inplace=True)
 new_colonnes.to_csv(r"dataset\LDC\ucl_clubs_attack.csv", index=False)
 
 
 df2 = pd.read_csv(r"dataset\LDC\ucl_players_attack.csv")
 new_colonnes = df2["team"].str.split(" ", expand=True)
 new_colonnes.columns = ["Rank", "Nom", "Equipe", "Tiret", "Poste", "Assist", "Corner_taken", "Offside", "Dribble", "Match_played"]
+new_colonnes = pd.read_csv(r"dataset\LDC\ucl_players_attack.csv")
+new_colonnes.drop("Tiret", axis=1, inplace=True)
 new_colonnes.to_csv(r"dataset\LDC\ucl_players_attack.csv", index=False)
